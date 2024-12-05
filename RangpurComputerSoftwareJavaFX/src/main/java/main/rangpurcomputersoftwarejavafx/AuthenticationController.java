0b3b6e705ae0a3a18e58e9db37f8b3dab40adc55
MahadiAhmed0 <mahadiahmed.2002@gmail.com>
2024-12-06 02:22:08 +0600

    added transition

--- RangpurComputerSoftwareJavaFX/src/main/java/main/rangpurcomputersoftwarejavafx/AuthenticationController.java
+++ RangpurComputerSoftwareJavaFX/src/main/java/main/rangpurcomputersoftwarejavafx/AuthenticationController.java
@@ -38,7 +38,38 @@ public class AuthenticationController implements Initializable {
     private SplitPane splitPane;
 
 
-    
+    public void switchForm(ActionEvent event) {
+        TranslateTransition slider = new TranslateTransition();
+        slider.setNode(coverForm);
+        slider.setDuration(Duration.seconds(0.3));
+        slider.setInterpolator(Interpolator.EASE_BOTH);
+
+        TranslateTransition slider2 = new TranslateTransition();
+        slider2.setNode(loginForm);
+        slider2.setDuration(Duration.seconds(0.3));
+        slider2.setInterpolator(Interpolator.EASE_BOTH);
+
+        double splitPaneWidth = splitPane.getWidth();
+
+        if (event.getSource() == forgetPasswordLink) {
+
+            slider.setToX(splitPaneWidth / 2);
+            slider2.setToX(splitPaneWidth / 2);
+
+            slider.setOnFinished(event1 -> slider2.play());
+        } else if (event.getSource() == back_Button) {
+            slider2.setToX(0);
+            slider.setToX(0);
+
+            slider2.setOnFinished(event1 -> slider.play());
+
+            slider2.play();
+        }
+        
+        if (event.getSource() == forgetPasswordLink) {
+            slider.play();
+        }
+    }
 
 
 
