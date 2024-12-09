92fa4aff3e9608e3fb75501040989c763f69d4d1
MahadiAhmed0 <mahadiahmed.2002@gmail.com>
2024-12-10 05:11:43 +0600

    all tagged with codename

--- RangpurComputerSoftwareJavaFX/src/main/java/main/rangpurcomputersoftwarejavafx/MainFormController.java
+++ RangpurComputerSoftwareJavaFX/src/main/java/main/rangpurcomputersoftwarejavafx/MainFormController.java
@@ -1,16 +1,228 @@
 package main.rangpurcomputersoftwarejavafx;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableView;
-
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 public class MainFormController {
    public class PleaseProvideControllerClassName {
 
-        @FXML
-        private TableView<?> inventory_table;
+           @FXML
+           private Button CrossButton;
+
+           @FXML
+           private AnchorPane Dashboard;
+
+           @FXML
+           private AnchorPane Inventory;
+
+           @FXML
+           private Button MaximizeButton;
+
+           @FXML
+           private Button MinimizeButton;
+
+           @FXML
+           private AnchorPane SaleHistory;
+
+           @FXML
+           private AnchorPane SellingPage;
+
+           @FXML
+           private AnchorPane add;
+
+           @FXML
+           private TextField addBrandName;
+
+           @FXML
+           private Button addButton;
+
+           @FXML
+           private TextField addCategory;
+
+           @FXML
+           private TextField addPrice;
+
+           @FXML
+           private TextField addProductName;
+
+           @FXML
+           private TextField addQuantity;
+
+           @FXML
+           private TextField addSubCategory;
+
+           @FXML
+           private Button add_AddButton;
+
+           @FXML
+           private Button add_ClearTextButton;
+
+           @FXML
+           private Label customerCount;
+
+           @FXML
+           private AnchorPane customerInfo;
+
+           @FXML
+           private Button customerInfoButtobn;
+
+           @FXML
+           private TextField customerSearchBar;
+
+           @FXML
+           private Button customerSearchButton;
+
+           @FXML
+           private TableView<?> customerTable;
+
+           @FXML
+           private Label dailyIncomeCount;
+
+           @FXML
+           private Label dailySaleCount;
+
+           @FXML
+           private Button dashboardButton;
+
+           @FXML
+           private AnchorPane delete;
+
+           @FXML
+           private Button deleteButton;
+
+           @FXML
+           private TextField deleteProductID;
+
+           @FXML
+           private Button delete_ClearTextButton;
+
+           @FXML
+           private Button delete_DeleteButton;
+
+           @FXML
+           private AnchorPane edit;
+
+           @FXML
+           private TextField editBrandName;
+
+           @FXML
+           private Button editButton;
+
+           @FXML
+           private TextField editCategory;
+
+           @FXML
+           private TextField editPrice;
+
+           @FXML
+           private TextField editProductID;
+
+           @FXML
+           private TextField editProductName;
+
+           @FXML
+           private TextField editQuantity;
+
+           @FXML
+           private TextField editSubCategory;
+
+           @FXML
+           private Button edit_ClearTextButton;
+
+           @FXML
+           private Button edit_EditButton;
+
+           @FXML
+           private Button inventoryButton;
+
+           @FXML
+           private Button inventorySearchButton;
+
+           @FXML
+           private TableView<?> inventory_table;
+
+           @FXML
+           private TextField inventorysearchBar;
+
+           @FXML
+           private Button logoutButton;
+
+           @FXML
+           private Label lowStockCount;
+
+           @FXML
+           private TableView<?> lowStock_table;
+
+           @FXML
+           private AnchorPane manageInventory;
+
+           @FXML
+           private Button manageInventoryButton;
+
+           @FXML
+           private TextField saleHistoryBar;
+
+           @FXML
+           private Button saleHistoryButton;
+
+           @FXML
+           private Button saleHistorySearchButton;
+
+           @FXML
+           private TableView<?> saleHistoryTable;
+
+           @FXML
+           private Button sellAddButton;
+
+           @FXML
+           private Button sellButton;
+
+           @FXML
+           private Button sellClearButton;
+
+           @FXML
+           private TextField sellDiscount;
+
+           @FXML
+           private Button sellProceedButton;
+
+           @FXML
+           private TextField sellProductID;
+
+           @FXML
+           private Button sellRemoveButton;
+
+           @FXML
+           private TableView<?> sellTable;
+
+           @FXML
+           private Label totalIncomeCount;
+
+           @FXML
+           private AnchorPane update;
+
+           @FXML
+           private Button updateButton;
+
+           @FXML
+           private TextField updatePrice;
+
+           @FXML
+           private TextField updateProductID;
+
+           @FXML
+           private TextField updateQuantity;
+
+           @FXML
+           private Button update_ClearTextButton;
+
+           @FXML
+           private Button update_UpdateButton;
+
         public void initialize() {
-           // Set the resize policy for the TableView
-           inventory_table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
        }
     }
 
